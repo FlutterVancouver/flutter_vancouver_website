77cd27e3364a2408c9cde40127d38ffefd7ce001
MarsGoatzz <sid.310@icloud.com>
2020-05-10 16:30:06 -0700

    Update Flutter web build

--- flutter_web_build/flutter_service_worker.js
+++ flutter_web_build/flutter_service_worker.js
@@ -5,7 +5,7 @@ const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "index.html": "48d771fbed876d80f77950ab1a1df361",
 "/": "48d771fbed876d80f77950ab1a1df361",
-"main.dart.js": "4c75536a6675e0372e34f8c67f22294e",
+"main.dart.js": "6746163d2234c0bee697bdd60352de7c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
